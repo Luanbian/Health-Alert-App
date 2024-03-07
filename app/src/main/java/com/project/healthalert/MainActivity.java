6a6ab267fab878e86265dd08867ad9ef79d5e6b6
Luan Bianchini <luanbianchine1@gmail.com>
2024-03-06 21:39:02 -0300

    first commit

---
+++ app/src/main/java/com/project/healthalert/MainActivity.java
@@ -0,0 +1,18 @@
+package com.project.healthalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Send(View view) {
+        System.out.println("vish é os guri");
+    }
+}
